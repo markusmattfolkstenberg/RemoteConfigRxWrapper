0e9a383c6d9d26fbc10f98183593b77d1fd4f19b
Markus Mattfolk Stenberg <markus.mattfolk@chas.se>
2018-09-06 17:18:23 +0200

    Making FirebaseRemoteConfig public.

--- RemoteConfigRxWrapper/Classes/FirebaseRemoteConfig.swift
+++ RemoteConfigRxWrapper/Classes/FirebaseRemoteConfig.swift
@@ -9,17 +9,17 @@ import Foundation
 import FirebaseRemoteConfig
 import RxSwift
 
-internal class FirebaseRemoteConfig: RemoteConfigType {
+public class FirebaseRemoteConfig: RemoteConfigType {
   var exiprationDuration: TimeInterval = 3600
   var remoteConfigInstance: RemoteConfig!
   
-  init(defaultValues: [String: NSObject]? = [: ], expirationDuration: TimeInterval = 3600, remoteConfigInstance: RemoteConfig = RemoteConfig.remoteConfig()) {
+  public init(defaultValues: [String: NSObject]? = [: ], expirationDuration: TimeInterval = 3600, remoteConfigInstance: RemoteConfig = RemoteConfig.remoteConfig()) {
     self.remoteConfigInstance = remoteConfigInstance
     self.exiprationDuration = expirationDuration
     self.remoteConfigInstance.setDefaults(defaultValues)
   }
   
-  func fetchValues() -> Observable<(RemoteConfigFetchStatus)> {
+  public func fetchValues() -> Observable<(RemoteConfigFetchStatus)> {
     return Observable.create {[unowned self] observer in
       self.remoteConfigInstance.fetch(withExpirationDuration: self.exiprationDuration, completionHandler: { status, error in
         if let error = error {
@@ -35,19 +35,19 @@ internal class FirebaseRemoteConfig: RemoteConfigType {
     }
   }
   
-  func activateFetchedValues() {
+  public func activateFetchedValues() {
     _ = remoteConfigInstance.activateFetched()
   }
   
-  func getBoolValue(with key: String) -> Bool {
+  public func getBoolValue(with key: String) -> Bool {
     return self.remoteConfigInstance.configValue(forKey: key).boolValue
   }
   
-  func getStringValue(with key: String) -> String? {
+  public func getStringValue(with key: String) -> String? {
     return self.remoteConfigInstance.configValue(forKey: key).stringValue
   }
   
-  func getNumberValue(with key: String) -> NSNumber? {
+  public func getNumberValue(with key: String) -> NSNumber? {
     return self.remoteConfigInstance.configValue(forKey: key).numberValue
   }
 }
